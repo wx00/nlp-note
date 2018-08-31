5acfa8edbfc2dd2aedf4de3d99d9cf7a91180417
Tommy <4850853+wx00@users.noreply.github.com>
2018-09-01 04:27:35 +0800

    dataset folder config based on platform

--- week1/__init__.py
+++ week1/__init__.py
@@ -1,15 +1,15 @@
 import logging
-import nltk
+# import nltk
+import platform
 
 log = logging.getLogger('Week1')
 
 # nltk.download('wordnet')
 # nltk.download('stopwords')
 
-
-# raw_base = "D:/Data/aclImdb/"
-raw_base = '/Users/pengshuolin/Downloads/aclImdb/'
-
-# feat_base = "D:/Data/imdb/"
-feat_base = '/Users/pengshuolin/data/imdb/'
-
+if platform.system() == 'Windows':
+    raw_base = "D:/Dataset/aclImdb/"
+    feat_base = "D:/Dataset/imdb/"
+else:
+    raw_base = '~/aclImdb/'
+    feat_base = '~/imdb/'
